6bdeb52587fc2fe5ebe22fd0164aca02a6ec7b30
hanshu <peter.hanshu@gmail.com>
2017-12-11 12:20:03 +0800

    add pass

--- find_code_leakage.py
+++ find_code_leakage.py
@@ -1,10 +1,14 @@
 import os
-
+import re
+pat=re.compile('.*\.com\W')
 def find_pitfall(fpath):
+    if fpath.endswith('.js'):
+        return
     for i,l in enumerate(open(fpath)):
         row=l.split()
         for el in row:
-            if el.find('.com')!=-1:
+            m=pat.match(el)
+            if m is not None:
                 print fpath,i+1,l
 
 for root,dirs,files in os.walk('.'):
