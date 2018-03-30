1e103ce3b9945e31ff964496d4eee8d3206e23f9
hanshu <peter.hanshu@gmail.com>
2018-03-30 15:18:13 +0800

    重构血缘装载

--- hql_dw_tools/hive_lineage/load_hql_meta.py
+++ hql_dw_tools/hive_lineage/load_hql_meta.py
@@ -1,4 +1,7 @@
+#!coding:utf8
 from pprint import pprint
+
+# 加表前缀函数缓存字典
 _TAP_CACHE={}
 def tblname_add_prefix( tblname):
     '''
@@ -17,48 +20,60 @@ def tblname_add_prefix( tblname):
             n_tblname= cpre+'.'+tblname
             _TAP_CACHE[tblname]=n_tblname
             return n_tblname
-    if tblname.startswith('tmp_'):
-        n_tblname= 'dw_tmp.'+tblname
-        _TAP_CACHE[tblname]=n_tblname
-        return n_tblname
     _TAP_CACHE[tblname]=tblname
     return tblname
 
-def filter_edge(src,tgt,midx):
-    if src.strip()=='' or tgt.strip()=='':
-        return True
-    if src.find('base_t')!=-1:
-        return True
+def is_skip_edge(src,tgt,reconnect_edge):
+    '''should the edge be skip,
+        and record parent and child nodes of the node
+    '''
+    is_skip=False
+    is_reonnect=False
+    ## 直接跳过节点
+    if src.strip()=='' or tgt.strip()=='':        
+        is_skip,stype= True,'empty_node_name'
+    if src.find('base_t')==0:
+        is_skip,stype= True,'with_alias'
+    ## 跳过，并记录上下游的节点
     if src.find('dw_tmp.')!=-1 or src.startswith('tmp_'):
-        midx['s_skip'].setdefault(src,set())
-        midx['s_skip'][src].add(tgt)
-        return True
+        reconnect_edge['up_edge'].setdefault(src,set())
+        reconnect_edge['up_edge'][src].add(tgt)
+        is_reonnect=True
+        is_skip,stype= True,'parent_is_temp'
     if tgt.find('dw_tmp.')!=-1 or tgt.startswith('tmp_'):
-        midx['t_skip'].setdefault(tgt,set())
-        midx['t_skip'][tgt].add(src)
-        return True
-    return False
+        reconnect_edge['down_edge'].setdefault(tgt,set())
+        reconnect_edge['down_edge'][tgt].add(src)
+        is_reonnect=True
+        is_skip,stype= True,'child_is_temp'
+    if is_skip:
+        action='Skip'
+        if is_reonnect:
+            action='Reconnect'
+        print '[%s Edge (%s)] %s->%s'%(action,stype,src,tgt)
+    return is_skip
 
-def get_min_len_midx(midx):
-    sl=len(midx['s_skip'])
-    tl=len(midx['t_skip'])
-    print '[Midx] source_skip:',sl,'target_skip:',tl
+def count_reconnect_edge(reconnect_edge):
+    '''
+    '''
+    sl=len(reconnect_edge['up_edge'])
+    tl=len(reconnect_edge['down_edge'])
+    # print '[Count reconnect_edge  length] up_edge:',sl,'down_edge:',tl    
     return min([sl,tl])
 
-def merge_midx(all_nodes,fedges,bedges,midx):
-    n_midx={'s_skip':{},'t_skip':{}}
+def reconnect_nodes(all_nodes,fedges,bedges,reconnect_edge):
+    n_reconnect_edge={'up_edge':{},'down_edge':{}}
     s_cnt=0
     t_cnt=0
     tm_cnt=0
-    for src,tgts in midx['s_skip'].items():
-        if src not in midx['t_skip']:
+    for src,tgts in reconnect_edge['up_edge'].items():
+        if src not in reconnect_edge['down_edge']:
             tm_cnt+=1
             continue
-        srcs=midx['t_skip'][src]
+        srcs=reconnect_edge['down_edge'][src]
         for src in srcs:
             for tgt in tgts:
-                is_f=filter_edge(src,tgt,n_midx)
-                if is_f:
+                is_skip=is_skip_edge(src,tgt,n_reconnect_edge)
+                if is_skip:
                     continue
                 fedges.setdefault(src,set())
                 bedges.setdefault(tgt,set())
@@ -66,48 +81,56 @@ def merge_midx(all_nodes,fedges,bedges,midx):
                     t_cnt+=1
                 if src not in bedges[tgt]:
                     s_cnt+=1
-                all_nodes.add(src)
-                all_nodes.add(tgt)
+                all_nodes.update([src,tgt])                
                 fedges[src].add(tgt)
                 bedges[tgt].add(src)                
-    print('[Merge_midx add edges]:src:',s_cnt,'tgt:',t_cnt)
-    print('[Terminal TempTable]:',tm_cnt)
-    get_min_len_midx(n_midx)
-    return n_midx
+    print '[Reconnect_edge add edges]:src:',s_cnt,'tgt:',t_cnt
+    print '[End at TempTable]:%s'%tm_cnt
+    return n_reconnect_edge
 
+def parse_nodes(st):
+    nodes=st.strip('" ').split(',')
+    nodes=map(tblname_add_prefix,nodes)
+    return nodes
+    
 def load_hql_meta(fname):
     all_nodes=set()
     fedges={}#forward
     bedges={}#backward   
-    midx={'s_skip':{},'t_skip':{}}
+    # merged index
+    reconnect_edge={'up_edge':{},'down_edge':{}}
     for line in open(fname):
         row=line.strip().split('\t')        
-        if len(row)<3:continue        
-        srcs=row[1].strip('" ').split(',')
-        tgts=row[2].strip('" ').split(',')
-        for src in srcs:
-            src=tblname_add_prefix(src)
-            for tgt in tgts:
-                tgt=tblname_add_prefix(tgt)
-                is_filter=filter_edge(src,tgt,midx)
-                if is_filter:
+        if len(row)<3:
+            # wrong format data
+            continue        
+        srcs=parse_nodes(row[1])
+        tgts=parse_nodes(row[2])        
+        for src in srcs:            
+            for tgt in tgts:                
+                is_skip=is_skip_edge(src,tgt,reconnect_edge)
+                if is_skip:
                     continue
                 fedges.setdefault(src,set())
                 bedges.setdefault(tgt,set())
                 fedges[src].add(tgt)
                 bedges[tgt].add(src)                
-                all_nodes.add(src)
-                all_nodes.add(tgt)
-    print('[Merge Index]')
-    res=get_min_len_midx(midx)
+                all_nodes.update([src,tgt])  
+    print '[Initial reconnect edges]'
+    pprint(reconnect_edge)
+    reconnect_count=count_reconnect_edge(reconnect_edge)
     i=0
-    while res>0 and i<10 :
+    while reconnect_count>0 and i<10 :
         #raw_input('[Loop]@',i)
         i+=1
-        midx=merge_midx(all_nodes,fedges,bedges,midx)
-        res=get_min_len_midx(midx)
+        reconnect_edge=reconnect_nodes(all_nodes,fedges,bedges,reconnect_edge)
+        reconnect_count=count_reconnect_edge(reconnect_edge)
+        print '[Reconnect loop:%s reonnect_count:%s]\n'%(i,reconnect_count)
     return all_nodes,fedges,bedges
 
 if __name__=='__main__':
     res=load_hql_meta("meta_test.xls")
+    names='all_node,forward,backward'.split(',')
+    res=zip(names,res)
+    print ''
     pprint(res)
